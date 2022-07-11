e8247994e833ab0a3d33970aa0166056d741bc9b
pengfeiyuan <ypf9319@gmail.com>
2022-07-11 22:27:40 +0800

    favicon

--- build.js
+++ build.js
@@ -31,6 +31,7 @@ const buildConfig = {
                     <meta charset="UTF-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
                     <link rel="stylesheet" href="/index.css">
+                    <link rel="icon" type="image/x-icon" href="/favicon.ico">
                 </head>
                 <body>
                     <div id="root">
